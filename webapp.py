9da9624bd3c7626df502aef531478eb328de1230
Mugo-Samuel <mugosamuelo5@gmail.com>
2026-07-02 16:40:44 +0300

    Prepare app for Koyeb deployment

--- webapp.py
+++ webapp.py
@@ -1,5 +1,6 @@
 import csv
 import io
+import os
 
 from flask import Flask, jsonify, render_template, request
 
@@ -74,4 +75,5 @@ def chat():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.getenv("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=True)
